d20d0f36d6505210e16c017a780e5ea03a906e1e
Sailesh Maharjan <saileshmaharjan9102@gmail.com>
2026-07-17 08:46:12 +0545

    Made some changes to font

--- src/html.js
+++ src/html.js
@@ -26,7 +26,7 @@ const Html = props => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Space+Mono:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Roboto+Mono:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
         <title>Sailesh Maharjan | Portfolio</title>
